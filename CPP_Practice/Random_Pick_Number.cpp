2873b59a8602864b6c261a8fe9f7b3dac9060c78
Park-Ji-Won <dpfmwptm01@gamil.com>
2025-09-15 13:27:22 +0900

    using namespace std;

--- CPP_Practice/Random_Pick_Number.cpp
+++ CPP_Practice/Random_Pick_Number.cpp
@@ -1,41 +1,42 @@
 ﻿#include <iostream>
 #include <random>
 #include <ctime>
+using namespace std;
 
 int main() {
-	std::mt19937 gen(time(0));
-	std::uniform_int_distribution<int> dis(1, 100);
+	mt19937 gen(time(0));
+	uniform_int_distribution<int> dis(1, 100);
 
 	int answer = dis(gen);
 	int guess = 0;
 	int attempts = 0;
 
-	std::cout << "숫자 추측 게임을 시작합니다!" << std::endl;
-	std::cout << "제가 생각한 1부터 100사이의 숫자를 맞춰보세요." << std::endl;
+	cout << "숫자 추측 게임을 시작합니다!" << endl;
+	cout << "제가 생각한 1부터 100사이의 숫자를 맞춰보세요." << endl;
 
 	while (true) {
-		std::cout << "\n숫자를 입력하세요.: ";
-		std::cin >> guess;
+		cout << "\n숫자를 입력하세요.: ";
+		cin >> guess;
 
-		if (std::cin.fail()) {
-			std::cout << "잘못된 입력입니다. 숫자만 입력해주십시오." << std::endl;
-			std::cin.clear();
-			std::cin.ignore(10000, '\n');
+		if (cin.fail()) {
+			cout << "잘못된 입력입니다. 숫자만 입력해주십시오." << endl;
+			cin.clear();
+			cin.ignore(10000, '\n');
 			continue;
 		}
 
 		attempts++;
 
 		if (guess < answer) {
-			std::cout << "더 큰 숫자입니다(Up)" << std::endl;
+			cout << "더 큰 숫자입니다(Up)" << endl;
 		}
 		else if (guess > answer) {
-			std::cout << "더 작은 숫자입니다(Down)" << std::endl;
+			cout << "더 작은 숫자입니다(Down)" << endl;
 		}
 		else {
-			std::cout << "\n 정답입니다. 축하합니다." << std::endl;
-			std::cout << "정답은" << answer << "이었습니다." << std::endl;
-			std::cout << attempts << "번 만에 맞춰습니다." << std::endl;
+			cout << "\n 정답입니다. 축하합니다." << endl;
+			cout << "정답은 " << answer << " 이었습니다." << endl;
+			cout << attempts << " 번 만에 맞춰습니다." << endl;
 			break;
 		}
 	}
