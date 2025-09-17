1e9dd25aa63539018e6273b7265c77f5d6db1fad
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-18 00:40:36 +0900

    CH02_LMS2 구현

---
+++ CPP_Practice_study_2/CH02_LMS02.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char line[100];
+
+	cout << "문자를 입력해 주세요 ";
+	cin.getline(line, 100, '\n');
+
+	int count = 0;
+
+	int length = strlen(line);
+
+	for (int i = 0; i < length; i++) {
+		if (line[i] == 'c' || line[i] == 'C') {
+			count++;
+		}
+	}
+
+	cout << "C 문자수 : " << count << endl;
+}
