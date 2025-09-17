a570b9c5db4a2ea371942ab43a5d2807c1ac717d
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-17 21:46:26 +0900

    ch02 응용연습 1단계

--- CPP_Practice_study_2/CH02_LMS_1.cpp
+++ CPP_Practice_study_2/CH02_LMS_1.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main() {
+	cout << "개인정보관리 프로그램" << endl;
+	cout << endl;
 
+	cout << "이름을 입력하세요: ";
+	char name[20];
+	cin >> name;
+
+	cout << "나이를 입력하세요: ";
+	int age;
+	cin >> age;
+
+	cout << endl;
+	cout << "입력된 정보" << endl;
+	cout << "이름: " << name << endl;
+	cout << "나이: " << age << " 세" << endl;
 }
