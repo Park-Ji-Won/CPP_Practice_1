815707591e5c79afb2530fe8c93c512bc75d59dd
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-25 01:58:51 +0900

    Circle Array의 동적 생성 및 활용

--- CPP_Practice_study_2/CH04_LMS02.cpp
+++ CPP_Practice_study_2/CH04_LMS02.cpp
@@ -19,5 +19,36 @@ Circle::Circle() {
 }
 
 int main() {
+	int n;
 
+	cout << "생성하고자 하는 원의 개수 : ";
+	cin >> n;
+
+	if (n < 0) {
+		cout << "프로그램을 종료합니다." << endl;
+		return 0;
+	}
+
+	Circle* pArray = new Circle[n];
+
+	for (int i = 0; i < n; i++) {
+		int r;
+		cout << "반지름 " << i + 1 << " : ";
+		cin >> r;
+		pArray[i].setRadius(r);
+	}
+
+	for (int i = 0; i < n; i++) {
+		double area = pArray[i].getArea();
+
+		cout << pArray[i].getArea() << "\t";
+		cout << (pArray + i)->getArea() << endl;
+
+		//if (area >= 100 && area <= 200) {
+			//cout << pArray[i].getArea() << "\t";
+			//cout << (pArray + i)->getArea() << endl;
+		//}
+	}
+
+	delete[] pArray;
 }
