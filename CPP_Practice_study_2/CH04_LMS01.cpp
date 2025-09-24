26bc234c3b138e4b7c8362ebec6bd9966ee35a13
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-25 00:47:51 +0900

    Circle 클래스의 배열 및 활용 출력 ver.1

---
+++ CPP_Practice_study_2/CH04_LMS01.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle() {
+		radius = 1;
+	}
+	Circle(int r) {
+		radius = r;
+	}
+	void setRadius(int r) {
+		radius = r;
+	}
+	double getArea();
+};
+
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
+
+int main() {
+
+}
