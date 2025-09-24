a824841bdd5476c46b0c205d66da494c1c99ab07
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-25 00:50:28 +0900

    Circle array 배열의 동적 생성 및 출력 활용

---
+++ CPP_Practice_study_2/CH04_LMS02.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle();
+	~Circle(){}
+	void setRadius(int r) {
+		radius = r;
+	}
+	double getArea() {
+		return 3.14 * radius * radius;
+	}
+};
+
+Circle::Circle() {
+	radius = 1;
+}
