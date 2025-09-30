e88c34b0ed7a1473beb42fa635f491010ad2b68c
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-01 03:51:45 +0900

    배열로 3가지 함수 호출 방식

---
+++ CPP_Practice_study_2/CH05_LMS1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+void cValue(int arr[5]) {
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		sum += arr[i];
+	}
+	cout << "cValue : " << sum << endl;
+}
+void cAddress(int* arr) {
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		sum += arr[i];
+	}
+	cout << "cAddress : " << sum << endl;
+}
+void cReference(int(&arr)[5]) {
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		sum += arr[i];
+	}
+	cout << "cReference : " << sum << endl;
+}
+
+int main() {
+	int arr[5] = { 1,2,3,4,5 };
+
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	cValue(arr);
+	cAddress(arr);
+	cReference(arr);
+
+	return 0;
+}
