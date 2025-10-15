0cc1cc974252cd0b461207499aa4d9d78452a96d
Park-Ji-Won <dpfmwptm01@gamil.com>
2025-10-15 13:19:36 +0900

    디폴트 매개 변수

---
+++ CPP_Practice_study_2/CH06_LMS1a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int add(int x[], int size, int y[] = NULL) {
+	int sum = 0;
+
+	for (int i = 0; i < size; i++) {
+		sum += x[i];
+	}
+
+	if (y != NULL) {
+		for (int i = 0; i < size; i++) {
+			sum += y[i];
+		}
+	}
+
+	return sum;
+}
+
+int main() {
+	int a[] = { 1,2,3,4,5,6 };
+	int b[] = { 6,7,8,9,10 };
+	int c = add(a, 5);
+	int d = add(a, 5, b);
+	cout << "c : " << c << endl;
+	cout << "d : " << d << endl;
+	return 0;
+}
